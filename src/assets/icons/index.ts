2df243a22fb9c68598f908233d6ab187ea807535
Mary <marypcng@hotmail.com>
2020-07-04 23:33:37 +1000

    export assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import Android192 from './android-chrome-192x192.png';
+import Android512 from './android-chrome-512x512.png';
+import Apple from './apple-touch-icon.png';
+import FavIcon16 from './favicon-16x16.png';
+import FavIcon32 from './favicon-32x32.png';
+import FavIcon from './favicon.ico';
+import Social from './social.png';
+
+export const icons = {
+  Android192,
+  Android512,
+  Apple,
+  FavIcon16,
+  FavIcon32,
+  FavIcon,
+  Social
+};
